0cb8842639d2a0c1d5f964b16a828ed4ab89794a
Johannes Ingrisch <johannes.ingrisch@uibk.ac.at>
2022-10-08 18:08:39 +0200

    Day 3 Part solved

--- Scripts/03.py
+++ Scripts/03.py
@@ -29,3 +29,61 @@ def solve_1(fn=input_test):
 
 
 solve_1(input_test)
+
+
+# Part 2
+#  Oxygen: determine most common value in bit position, keep only elements with this value
+#  if values are equal, keep 0
+
+def oxygen_rating(diag_input):
+    n = len(diag_input[0])
+    l = len(diag_input)
+
+    for ind in range(0, n):
+        d = [int(el[ind]) for el in diag_input]
+        if sum(d)/len(d) >= 0.5:
+            diag_input = [diag_input[i]
+                          for i in range(len(diag_input)) if d[i] == 1]
+        else:
+            diag_input = [diag_input[i]
+                          for i in range(len(diag_input)) if d[i] == 0]
+
+    rating = int(diag_input[0], base=2)
+    return (rating)
+
+
+def co2_rating(diag_input):
+    n = len(diag_input[0])
+    l = len(diag_input)
+
+    for ind in range(0, n):
+        d = [int(el[ind]) for el in diag_input]
+        if sum(d)/len(d) < 0.5:
+            diag_input = [diag_input[i]
+                          for i in range(len(diag_input)) if d[i] == 1]
+        else:
+            diag_input = [diag_input[i]
+                          for i in range(len(diag_input)) if d[i] == 0]
+        print(diag_input)
+        if len(diag_input) == 1:
+            break
+    rating = int(diag_input[0], base=2)
+    return (rating)
+
+
+def life_rating(diag_input):
+    oxygen = oxygen_rating(diag_input)
+    co2 = co2_rating(diag_input)
+    life = oxygen * co2
+    return (life)
+
+
+def solve_2(fn=input_test):
+    with open(fn) as f:
+        diagnostics = [el[:-1] for el in f.readlines()]
+
+    print("Life support rating:", life_rating(diagnostics))
+
+
+solve_2(input_test)
+solve_2(input_full)
