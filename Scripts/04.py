7dbccfe29cdfde0e275a5360157f28d2066dafa5
Johannes Ingrisch <johannes.ingrisch@uibk.ac.at>
2022-11-03 13:49:51 +0100

    finished Day4 Part 2

--- Scripts/04.py
+++ Scripts/04.py
@@ -1,6 +1,5 @@
 # Day 4
 
-# Part 1 -------
 
 import numpy as np
 import Scripts.funs_04 as f
@@ -11,16 +10,16 @@ input_test = 'Data/04_test.txt'
 input_full = 'Data/04_full.txt'
 
 fn = input_full
-
 # read called numbers
 called = f.read_called(fn)
 
 # read the bingo boards
 boarddata = f.edit_boards(fn)
 
+# Part 1 -------
+
 # transform to class 'board'
 all_boards = [board(np.array(el).astype(int)) for el in boarddata]
-
 
 # Playing the game ++++++
 
@@ -40,4 +39,36 @@ while winner == 0:
             winsum = all_boards[i].sum
             winval = winsum * called_number
             print(f"The win value is {winval}")
+    round += 1
+
+
+# Part 2 -------
+
+
+# transform to class 'board'
+all_boards = [board(np.array(el).astype(int)) for el in boarddata]
+
+# Playing the game ++++++
+
+winner = 0
+round = 0
+ind = list(range(0, len(all_boards)))
+total_wins = 0
+
+while total_wins < len(all_boards):
+    called_number = called[round]
+    print(f"Round {round + 1} of the game")
+    print(f"We draw number {called_number}")
+    for i in ind:
+        if all_boards[i].win == False:
+            all_boards[i].call_match(called_number)
+            all_boards[i].win_value()
+            if all_boards[i].win == True:
+                total_wins += 1
+                print(f"Board #{i} finished")
+                all_boards[i].undrawn_sum()
+                winsum = all_boards[i].sum
+                print(f"undrawn sum: {winsum}")
+                winval = winsum * called_number
+                print(f"The win value is {winval}")
     round += 1
